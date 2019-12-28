f402fa014a3fe4a4e5ee68f530bee22e96ae5fff
SpIIIII <Shipin16@gmail.com>
2019-12-28 14:55:43 +0300

    v1.0.2 move and rename version file

--- version/version.py
+++ version/version.py
@@ -5,7 +5,7 @@ class Versions:
         self.ulr_of_version = "http://68.183.208.74"
         self.remote_version_txt = self.get_remote_version_txt()
         self.remote_version = self.version_to_int(self.remote_version_txt)
-        with open('version/version', 'r') as f:
+        with open('.version', 'r') as f:
             self.local_version_txt = f.read()
         self.local_version = self.version_to_int(self.local_version_txt)
         
