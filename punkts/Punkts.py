af9364801e70051fb52a4ce0fd1759ed236984ad
SpIIIII <Shipin16@gmail.com>
2020-01-13 14:25:44 +0300

    v1.0.6 change version file and delete unnecessarily prints

--- punkts/Punkts.py
+++ punkts/Punkts.py
@@ -78,7 +78,6 @@ class Punkt:
             tk.messagebox.showinfo('Готово',f'Пункт {self.name} удален')
 
     def update(self, *args)-> None:
-        print(args)
         self.db.update_data(*args)
 
     def __str__(self)-> None:
